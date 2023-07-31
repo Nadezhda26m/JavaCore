f8b45557eff04fe512bf7eb1c9ea19280dee0be9
Nadezhda26m <nadezhda-26m@mail.ru>
2023-07-31 20:17:53 +0300

    hw 2.2 add new methode encode Шифр Цезаря)

--- Homework2/src/main/java/ru/core/task2/Main02.java
+++ Homework2/src/main/java/ru/core/task2/Main02.java
@@ -5,12 +5,14 @@ package ru.core.task2;
 
 public class Main02 {
     public static void main(String[] args) {
-        // String string = "абвгдежзиклмн";
-        String string = "здесьдолженбытьtext";
+        String alphabetLowCase = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+        String string = "здеСь.должен/быТь теКст";
         System.out.println(string);
         String yes = ceasar(string, 4, true);
-        System.out.println(yes); // лийхѐитпкйсеяцѐxi|x
+        System.out.println(yes); // лийХѐ2итпкйс3еяЦѐ$цйОхц
         System.out.println(ceasar(yes, 4, false));
+        System.out.println(shiftCipher(string, alphabetLowCase, 3, true));
+        // кжзФя.жсойзр/дюХя хзНфх
     }
 
     public static String ceasar(String in, int key, boolean encrypt) {
@@ -22,5 +24,34 @@ public class Main02 {
             out[i] = (char) (in.charAt(i) + ((encrypt) ? key : -key));
         }
         return new String(out);
+    }
+
+    public static String shiftCipher(String original, String alphabet,
+                                     int countShift, boolean encode) {
+        char[] characters = new char[alphabet.length()];
+        alphabet.getChars(0, alphabet.length(), characters, 0);
+        char[] result = new char[original.length()];
+        String downOriginal = original.toLowerCase();
+        char newChar;
+        boolean isLetter;
+        for (int i = 0; i < result.length; i++) {
+            isLetter = false;
+            for (int j = 0; j < characters.length; j++) {
+                if (downOriginal.charAt(i) == characters[j]) {
+                    // encode = true
+                    newChar = characters[(j + countShift) % characters.length];
+                    if (original.charAt(i) != downOriginal.charAt(i)) {
+                        result[i] = Character.toUpperCase(newChar);
+                    } else {
+                        result[i] = newChar;
+                    }
+                    isLetter = true;
+                }
+            }
+            if (!isLetter) {
+                result[i] = original.charAt(i);
+            }
+        }
+        return new String(result);
     }
 }
